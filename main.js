2e3d8b6df307d621e83364e3559779db184d6723
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-02-17 16:27:59 +0700

    [ADD] Filter for Search & show "Not Found" when not found

--- main.js
+++ main.js
@@ -1,54 +1,18 @@
 const app = {
   data() {
     return {
-      thaides: [{
-          name: 'Bu Lan Dan Mek',
-          url: './images/1.jpg',
-          isLike: false
-        },
-        {
-          name: 'Bu Lan Dan Mhok',
-          url: './images/2.jpg',
-          isLike: false
-        },
-        {
-          name: 'Ja Mong Kut (Original)',
-          url: './images/3.jpg',
-          isLike: false
-        },
-        {
-          name: 'Ja Mong Kut (Now)',
-          url: './images/4.jpg',
-          isLike: false
-        },
-        {
-          name: 'Leum Guen',
-          url: './images/5.jpg',
-          isLike: false
-        },
-        {
-          name: 'Phra Pai',
-          url: './images/6.jpg',
-          isLike: false
-        },
-        {
-          name: 'Cho maung',
-          url: './images/7.jpg',
-          isLike: false
-        },
-        {
-          name: 'Look Chub',
-          url: './images/8.jpg',
-          isLike: false
-        },
-        {
-          name: 'Thong Eak',
-          url: './images/9.jpg',
-          isLike: false
-        }
-      ],
+      thaides: [{name: 'Bu Lan Dan Mek',url: './images/1.jpg',isLike: false},
+                {name: 'Bu Lan Dan Mhok',url: './images/2.jpg',isLike: false},
+                {name: 'Ja Mong Kut (Original)',url: './images/3.jpg',isLike: false},
+                {name: 'Ja Mong Kut (Now)',url: './images/4.jpg',isLike: false},
+                {name: 'Leum Guen',url: './images/5.jpg',isLike: false},
+                {name: 'Phra Pai',url: './images/6.jpg',isLike: false},
+                {name: 'Cho maung',url: './images/7.jpg',isLike: false},
+                {name: 'Look Chub',url: './images/8.jpg',isLike: false},
+                {name: 'Thong Eak',url: './images/9.jpg',isLike: false}],
       inputs: '',
-      searchBoxOpen: false
+      searchBoxOpen: false,
+      searchFound:false
     }
   },
   methods: {
@@ -68,13 +32,12 @@ const app = {
       return this.thaides.filter(t => t.isLike).length
     },
     filteredList() {
-      searchFound = true
-      if (this.inputs) {
-        return this.thaides.filter(t => {return this.inputs.toLowerCase().split(' ').every(v => t.name.toLowerCase().includes(v))})
-      }else if(!this.inputs){
-        return this.thaides;
-      }else{
-        return 
+      this.searchFound = false
+      if (this.inputs == '') { return this.thaides } 
+      else {
+        let f = this.thaides.filter(t => this.inputs.toLowerCase().split(' ').every(v => t.name.toLowerCase().includes(v)))
+        if (f == '') {this.searchFound = true}
+        else {return f}
       }
     }
   }
