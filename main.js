0d787e9418dcd78b910e3ed78e61ed0a66733e2f
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-02-20 10:47:35 +0700

    [ADD] Preview Images & [END] Groupwork4

--- main.js
+++ main.js
@@ -1,18 +1,57 @@
 const app = {
   data() {
     return {
-      thaides: [{name: 'Bu Lan Dan Mek',url: './images/1.jpg',isLike: false},
-                {name: 'Bu Lan Dan Mhok',url: './images/2.jpg',isLike: false},
-                {name: 'Ja Mong Kut (Original)',url: './images/3.jpg',isLike: false},
-                {name: 'Ja Mong Kut (Now)',url: './images/4.jpg',isLike: false},
-                {name: 'Leum Guen',url: './images/5.jpg',isLike: false},
-                {name: 'Phra Pai',url: './images/6.jpg',isLike: false},
-                {name: 'Cho maung',url: './images/7.jpg',isLike: false},
-                {name: 'Look Chub',url: './images/8.jpg',isLike: false},
-                {name: 'Thong Eak',url: './images/9.jpg',isLike: false}],
+      thaides: [{
+          name: 'Bu Lan Dan Mek',
+          url: './images/1.jpg',
+          isLike: false
+        },
+        {
+          name: 'Bu Lan Dan Mhok',
+          url: './images/2.jpg',
+          isLike: false
+        },
+        {
+          name: 'Ja Mong Kut (Original)',
+          url: './images/3.jpg',
+          isLike: false
+        },
+        {
+          name: 'Ja Mong Kut (Now)',
+          url: './images/4.jpg',
+          isLike: false
+        },
+        {
+          name: 'Leum Guen',
+          url: './images/5.jpg',
+          isLike: false
+        },
+        {
+          name: 'Phra Pai',
+          url: './images/6.jpg',
+          isLike: false
+        },
+        {
+          name: 'Cho maung',
+          url: './images/7.jpg',
+          isLike: false
+        },
+        {
+          name: 'Look Chub',
+          url: './images/8.jpg',
+          isLike: false
+        },
+        {
+          name: 'Thong Eak',
+          url: './images/9.jpg',
+          isLike: false
+        }
+      ],
       inputs: '',
       searchBoxOpen: false,
-      searchFound:false
+      searchFound: false,
+      showImages: false,
+      currentIndex: 0
     }
   },
   methods: {
@@ -25,7 +64,16 @@ const app = {
     closeSearch() {
       this.searchBoxOpen = !this.searchBoxOpen
       this.inputs = ''
+    },
+    showImg(index) {
+      this.showImages = true;
+      this.currentIndex = index;
+    },
+    hideImg(index) {
+      this.showImages = false;
+      this.currentIndex = index;
     }
+    
   },
   computed: {
     countLike() {
@@ -33,13 +81,18 @@ const app = {
     },
     filteredList() {
       this.searchFound = false
-      if (this.inputs == '') { return this.thaides } 
-      else {
+      if (this.inputs == '') {
+        return this.thaides
+      } else {
         let f = this.thaides.filter(t => this.inputs.toLowerCase().split(' ').every(v => t.name.toLowerCase().includes(v)))
-        if (f == '') {this.searchFound = true}
-        else {return f}
+        if (f == '') {
+          this.searchFound = true
+        } else {
+          return f
+        }
       }
     }
+
   }
 
 }
